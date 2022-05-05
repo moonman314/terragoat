f97d19f713112df6a8734541652127499d991903
moonman314 <86838014+moonman314@users.noreply.github.com>
2022-05-05 10:56:12 -0400

    Update terraform/azure/Azurecodereview.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/azure/Azurecodereview.tf
+++ terraform/azure/Azurecodereview.tf
@@ -45,6 +45,9 @@ resource azurerm_app_service "app-service1" {
 }
 
 resource azurerm_app_service "app-service2" {
+  site_config {
+    ftps_state = "Disabled"
+  }
   app_service_plan_id = azurerm_app_service_plan.example.id
   location            = var.location
   name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
